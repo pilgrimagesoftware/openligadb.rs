efc74a68071f12bd46c3f3134390120a28c4f030
Paul Schifferer <paul@schifferers.net>
2025-02-10 06:53:16 -0800

    Add list function and test for League

--- src/models/league.rs
+++ src/models/league.rs
@@ -1,5 +1,9 @@
 use crate::models::sport::Sport;
+use crate::{client::List, constants::API_BASE_URL};
+use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
+use std::error::Error;
+use url::Url;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct League {
@@ -12,4 +16,27 @@ pub struct League {
     #[serde(rename(deserialize = "leagueSeason"))]
     pub season: String,
     pub sport: Sport,
+}
+
+#[async_trait]
+impl<M: for<'de> serde::Deserialize<'de> + 'static> List<M> for League {
+    async fn list() -> Result<Vec<M>, Box<dyn Error>> {
+        let api_url = Url::parse(&format!("{}/getavailableleagues", API_BASE_URL))?;
+
+        crate::client::list::<M>(api_url).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::error::Error;
+
+    #[actix_web::test]
+    async fn test_list_leagues() {
+        let leagues: Result<Vec<League>, Box<dyn Error>> = League::list().await;
+        dbg!(&leagues);
+
+        assert!(leagues.is_ok());
+    }
 }
