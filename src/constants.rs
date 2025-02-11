06ff3ad6dcfb7ab834e2a3bfe55582a14dbd66a5
Paul Schifferer <paul@schifferers.net>
2025-02-11 08:53:57 -0800

    Docs

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,7 @@
+/**
+ * Constants
+ * @paulyhedral
+ */
+
+/// The default URL for the OpenLigaDB API.
 pub const API_BASE_URL : &str = "https://api.openligadb.de";
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! A Rust library for accessing the [OpenLigaDB] [API].
+//!
+//! [OpenLigaDB]: https://www.openligadb.de/
+//! [API]: https://api.openligadb.de/index.html
+
+#![allow(
+    clippy::doc_markdown,
+    clippy::module_name_repetitions,
+    clippy::must_use_candidate,
+    clippy::option_if_let_else
+)]
+#![warn(missing_docs)]
+#![warn(rust_2018_idioms)]
+
+mod constants;
 mod models;
 mod util;
-mod constants;
